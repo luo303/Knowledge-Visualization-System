6dbed7c1a88c824d373bc13c93cd1f6862e6cf70
xhh <jiatangxi@qq.com>
2025-10-28 22:17:26 +0800

    chore: 暂时提交以切换分支

--- src/views/auth/SystemRegister.vue
+++ src/views/auth/SystemRegister.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>我是注册页面</div>
+  <div class="register">
+    <div class="content">
+      <div class="content-left">fdfd</div>
+      <div class="content-right">dfdf</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.register {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: $before_login_background;
+  .content {
+    width: 70%;
+    height: 65vh;
+    border-radius: 40px;
+    background-color: aliceblue;
+    padding: 20px;
+    display: flex;
+    gap: 5px;
+    .content-left {
+      flex: 1;
+    }
+    .content-right {
+      flex: 1;
+    }
+  }
+}
+</style>
