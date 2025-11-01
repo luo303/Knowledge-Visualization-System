f2ba690fac6d2cddcab1663086a026187594ec12
xhh <jiatangxi@qq.com>
2025-11-01 15:11:23 +0800

    style: 固定盒子大小

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -5,7 +5,7 @@ import type { forgetpwd, code, registerData } from './type'
 
 //注册
 export const Register = (data: registerData) =>
-  request.post(' /api/biz/v1/user/register', data)
+  request.post('/api/biz/v1/user/register', data)
 //忘记密码
 export const Forgetpwd = (data: forgetpwd) =>
   request.post('/api/biz/v1/user/reset_password', data)
